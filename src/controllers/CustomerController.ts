97ae715a1ccc86df2df3040003c50a7c337a4320
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2022-10-14 18:51:40 -0500

    4: make it green

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,10 +1,16 @@
+import { mockedCustomers } from "../mock/mockedCustomers";
 import {Request, Response} from "express"
 
 export const getCustomers = (request: Request, response: Response) => {
-
+    return response
+        .status(200)
+        .send(mockedCustomers)
 }
 
 export const createCustomer = (request: Request, response: Response) => {
- 
+    mockedCustomers.push(request.body)
+    return response
+        .status(201)
+        .send()
 }
 
