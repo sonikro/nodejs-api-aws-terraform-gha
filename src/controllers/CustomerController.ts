989ad5fbdb09451d67c09b0d5c9013048c25184e
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2022-10-14 18:51:37 -0500

    3: tests first

---
+++ src/controllers/CustomerController.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from "express"
+
+export const getCustomers = (request: Request, response: Response) => {
+
+}
+
+export const createCustomer = (request: Request, response: Response) => {
+ 
+}
+
